ee2e0264f02603956852d1ee938cdb42aaf0c298
Ami Buch <64002199+AmiBuch@users.noreply.github.com>
2020-08-17 03:05:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def main():
         input_variables = pd.DataFrame([[oneM, twoM]], columns = ['oneM', 'twoM'], dtype=string)
         
         
-        return flask.render_template('main.html', original_input = {'Words that are important in the answer (1 mark)':oneM, 'Words that are important in the answer (2 mark)':twoM}, result = count*100/total,)
+        return flask.render_template('main.html', original_input = {'Words that are important in the answer (1 mark)':oneM, 'Words that are important in the answer (2 mark)':twoM}, result = str(count) + "from" + str(total),)
 
 if __name__ == '__main__':
     app.run()
